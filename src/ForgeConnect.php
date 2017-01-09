d2a24290f16a8cc1e2f4687bfef282a009b7a539
LKDevelopment <kontakt@lukas-kaemmerling.de>
2017-01-09 17:36:31 +0000

    Apply fixes from StyleCI

--- src/ForgeConnect.php
+++ src/ForgeConnect.php
@@ -3,27 +3,22 @@
  * Created by PhpStorm.
  * User: lukaskammerling
  * Date: 14.12.16
- * Time: 15:08
+ * Time: 15:08.
  */
 
 namespace LKDevelopment\ForgeConnect;
 
-
-use LKDevelopment\ForgeConnect\Commands\ConnectToServerCommand;
-use LKDevelopment\ForgeConnect\Commands\ListServersCommand;
-use LKDevelopment\ForgeConnect\Commands\RegisterCommand;
 use Symfony\Component\Console\Application;
+use LKDevelopment\ForgeConnect\Commands\RegisterCommand;
+use LKDevelopment\ForgeConnect\Commands\ListServersCommand;
+use LKDevelopment\ForgeConnect\Commands\ConnectToServerCommand;
 
 /**
- * Class ForgeConnect
- * @package LKDevelopment\ForgeConnect
+ * Class ForgeConnect.
  */
 class ForgeConnect extends Application
 {
-    /**
-     *
-     */
-    CONST VERSION = '1.2.0';
+    const VERSION = '1.2.0';
 
     /**
      * ForgeConnect constructor.
