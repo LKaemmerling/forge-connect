3ca345d2a314e8cf4382aad1c336df4c45222535
helio <insign@gmail.com>
2017-01-10 17:22:09 -0300

    better OS treatment

--- src/Traits/InteractsWithForgeConfiguration.php
+++ src/Traits/InteractsWithForgeConfiguration.php
@@ -9,6 +9,7 @@
 namespace LKDevelopment\ForgeConnect\Traits;
 
 use Illuminate\Encryption\Encrypter;
+use insign\getOS;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -18,106 +19,113 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 trait InteractsWithForgeConfiguration
 {
-    /**
-     * @var
-     */
-    protected $passphrase;
+   /**
+    * @var
+    */
+   protected $passphrase;
 
-    /**
-     * Get the Forge Credentials from the configuration file.
-     *
-     * @return string
-     * @throws \Exception
-     */
-    protected function readConfig()
-    {
-        if (! $this->configExists()) {
-            throw new \Exception('Forge Connect configuration file not found. Please register a Credentials.');
-        }
+   /**
+    * Get the Forge Credentials from the configuration file.
+    *
+    * @return string
+    * @throws \Exception
+    */
+   protected function readConfig()
+   {
+      if (!$this->configExists()) {
+         throw new \Exception('Forge Connect configuration file not found. Please register a Credentials.');
+      }
 
-        return json_decode(
-            file_get_contents($this->configPath()), true
-        );
-    }
+      return json_decode(file_get_contents($this->configPath()), TRUE);
+   }
 
-    /**
-     * @param InputInterface $input
-     * @param OutputInterface $output
-     */
-    protected function askForPassphrase(InputInterface $input, OutputInterface $output)
-    {
-        $helper = $this->getHelper('question');
+   /**
+    * @param InputInterface $input
+    * @param OutputInterface $output
+    */
+   protected function askForPassphrase(InputInterface $input, OutputInterface $output)
+   {
+      $helper = $this->getHelper('question');
 
-        $question = new Question('Please enter your passphrase:');
-        $question->setHidden(true);
-        $this->passphrase = hash('md5', $helper->ask($input, $output, $question));
-    }
+      $question = new Question('Please enter your passphrase:');
+      $question->setHidden(TRUE);
+      $this->passphrase = hash('md5', $helper->ask($input, $output, $question));
+   }
 
-    /**
-     * @return mixed
-     */
-    public function getCredentials()
-    {
-        $encryption = new Encrypter($this->passphrase, 'AES-256-CBC');
-        $pass = $encryption->decrypt($this->readConfig()['credentials']['password']);
+   /**
+    * @return mixed
+    */
+   public function getCredentials()
+   {
+      $encryption = new Encrypter($this->passphrase, 'AES-256-CBC');
+      $pass       = $encryption->decrypt($this->readConfig()[ 'credentials' ][ 'password' ]);
 
-        return [
-            'email' => $this->readConfig()['credentials']['email'],
-            'password' => $pass,
-        ];
-    }
+      return [
+          'email'    => $this->readConfig()[ 'credentials' ][ 'email' ],
+          'password' => $pass,
+      ];
+   }
 
-    /**
-     * Write the Forge Credentials to the configuration.
-     *
-     * @param  string $token
-     * @return void
-     */
-    protected function storeCredentials($email, $password)
-    {
-        $encryption = new Encrypter($this->passphrase, 'AES-256-CBC');
-        $config = ($this->configExists()) ? $this->readConfig() : [];
-        $config['credentials'] = [
-            'email' => $email,
-            'password' => $encryption->encrypt($password),
-        ];
-        $this->storeConfig($config);
-    }
+   /**
+    * Write the Forge Credentials to the configuration.
+    *
+    * @param  string $token
+    * @return void
+    */
+   protected function storeCredentials($email, $password)
+   {
+      $encryption              = new Encrypter($this->passphrase, 'AES-256-CBC');
+      $config                  = ($this->configExists()) ? $this->readConfig() : [];
+      $config[ 'credentials' ] = [
+          'email'    => $email,
+          'password' => $encryption->encrypt($password),
+      ];
+      $this->storeConfig($config);
+   }
 
-    /**
-     * @param $config
-     */
-    protected function storeConfig($config)
-    {
-        file_put_contents($this->configPath(), json_encode($config, JSON_PRETTY_PRINT).PHP_EOL);
-    }
+   /**
+    * @param $config
+    */
+   protected function storeConfig($config)
+   {
+      file_put_contents($this->configPath(), json_encode($config, JSON_PRETTY_PRINT) . PHP_EOL);
+   }
 
-    /**
-     * Get the Forge Connect configuration file path.
-     *
-     * @return string
-     */
-    protected function configPath()
-    {
-        return $this->getPath().'/config.json';
-    }
+   /**
+    * Get the Forge Connect configuration file path.
+    *
+    * @return string
+    */
+   protected function configPath()
+   {
+      return $this->getPath() . '/config.json';
+   }
 
-    /**
-     * Determine if the Forge Connect configuration file exists.
-     *
-     * @return bool
-     */
-    protected function configExists()
-    {
-        return file_exists($this->configPath());
-    }
+   /**
+    * Determine if the Forge Connect configuration file exists.
+    *
+    * @return bool
+    */
+   protected function configExists()
+   {
+      return file_exists($this->configPath());
+   }
 
-    protected function getConsoleTool()
-    {
-        if (! isset($this->readConfig()['console'])) {
+   protected function getConsoleTool()
+   {
+
+
+      if (isset($this->readConfig()[ 'console' ])) {
+         return $this->readConfig()[ 'console' ];
+      }
+
+      switch (getOS::ofServer()) {
+         case 4: // OSX
             return 'open ssh://{user}@{ip_address}';
-        } else {
-            return $this->readConfig()['console'];
-        }
-    }
+            break;
+         default: // Linux and others (for now)
+            return 'ssh {ip_address} -l {user}';
+            break;
+      }
+   }
 }
